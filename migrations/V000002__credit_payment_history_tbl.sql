66d9c0989fef3514ea79be475b9efbfec373e1ed
Prudhvi <paddepalli@confluent.io>
2022-12-12 01:01:24 -0600

    new table

---
+++ migrations/V000002__credit_payment_history_tbl.sql
@@ -0,0 +1,5 @@
+CREATE TABLE credit_payment_history_tbl AS
+    SELECT customer_id,
+          ROUND(CAST(SUM(paid_on_time) AS DOUBLE) / CAST(COUNT(*) AS DOUBLE) * 100, 2) as PCT_PAID_ON_TIME
+    FROM credit_payment_history
+    GROUP BY customer_id;
