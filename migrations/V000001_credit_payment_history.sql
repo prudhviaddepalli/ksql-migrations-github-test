cbff4bf0ab2679ff46e44984823efa27be59a340
Prudhvi <paddepalli@confluent.io>
2022-12-12 00:06:58 -0600

    first stream

---
+++ migrations/V000001_credit_payment_history.sql
@@ -0,0 +1,9 @@
+SET 'auto.offset.reset'='earliest';
+
+CREATE STREAM credit_payment_history (customer_id VARCHAR,
+                     credit_type VARCHAR,
+                     paid_on_time INT)
+                         WITH (KAFKA_TOPIC='mysql_credit_payment_history',
+              VALUE_FORMAT='JSON',
+              KEY_FORMAT='JSON');
+
