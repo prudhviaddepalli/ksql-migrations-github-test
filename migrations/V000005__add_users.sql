6257c6350eb78b5c4da9551d619f6bd1867e2290
Prudhvi <paddepalli@confluent.io>
2023-03-24 09:05:38 -0500

    demostream

--- migrations/V000005__add_users.sql
+++ migrations/V000005__add_users.sql
@@ -3,6 +3,6 @@ SET 'auto.offset.reset'='earliest';
 CREATE or replace STREAM add_users (customer_id VARCHAR,
                      credit_type VARCHAR,
                      paid_on_time INT)
-                         WITH (KAFKA_TOPIC='mysql_credit_payment_history',
+                         WITH (KAFKA_TOPIC='mysql_credit_payment_history2',
               VALUE_FORMAT='JSON',
               KEY_FORMAT='JSON');
